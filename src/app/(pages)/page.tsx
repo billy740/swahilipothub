33d702088c15736d3d14b1efb1501f69fbe5e639
Mwamtindi <mwamtindishaban@gmail.com>
2025-03-07 10:02:24 +0300

    update navbar

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -5,12 +5,14 @@ import BecomeMember from "@/components/member";
 import Numbers from "@/components/numbers";
 import SpacesCard from "@/components/spacescard";
 import Swahilipotfm from "@/components/spfm";
+import JoinYouth from "@/components/youth";
 
 export default function DashboardPage() {
     return (
       <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
         <HeroNew />
         <Swahilipotfm />
+        <JoinYouth />
         <CaseManagement />
         <SpacesCard />
         <Numbers />
